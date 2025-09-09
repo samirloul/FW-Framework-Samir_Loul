8ce77ed85bae3a255f0fc417d0ac4fb2dea0f8d3
samirloul <sameerloul2010@gmail.com>
2025-09-09 14:16:58 +0200

    ik ben bij les 3 gekomen van leren laravel in 30 dagen

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About page</title>
+</head>
+<body>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    </nav>
+   <h1>
+     hello this is about page
+     </h1>
+</body>
+</html>
