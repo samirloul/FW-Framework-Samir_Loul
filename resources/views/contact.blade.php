0eddf2e51022f2ddc7f84c92356543ecac06a34c
samirloul <sameerloul2010@gmail.com>
2025-09-15 10:39:34 +0200

    les 4 ik heb geleerd in les 4 hoe kan ik appart file voor nav maken en eigen vriable maken en hoe kan html van tailwindcss en css kan gebruiken

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,7 @@
 <x-layout>
+    <x-slot:heading>
+    Home page
+</x-slot:heading>
     <h1> hello this is contact page</h1>
 </x-layout>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,8 @@
+
 <x-layout>
+<x-slot:heading>
+    Home page
+</x-slot:heading>
     <h1> hello this is home page</h1>
 </x-layout>
 
